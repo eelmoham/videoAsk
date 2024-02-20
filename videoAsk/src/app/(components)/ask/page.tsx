7bd3d2ac088c9f8926c95e88e3170b6f600eb979
eelmoham <mohammdymehdi@gmail.com>
2024-02-20 03:39:55 +0100

    algorithm for search and in the tree

--- videoAsk/src/app/(components)/ask/page.tsx
+++ videoAsk/src/app/(components)/ask/page.tsx
@@ -2,159 +2,126 @@
 import { FC, useState, useEffect } from "react";
 // import Player from "./player";
 import dynamic from 'next/dynamic';
+import { text } from "stream/consumers";
+import Player from "./player";
 
 const DynamicReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 interface Video {
+    // id: string;
     url: string;
     options: Option[];
 }
 
 interface Option {
-    text: string;
+    text: string | null;
     vidio: Video | null;
 }
 
 interface AskProps {
-    videos: Video[];
+    // videos: Video[];
 }
 
-const videoLinks = ["https://youtu.be/B4SX5FFE2XE?si=gZxq4NxgXJlZehiZ",
-    "https://youtu.be/vnFybzZtVoQ?si=s05FnYS84z6D7Uxn",
-    "https://youtu.be/bz_DPnBEffw?si=-O_eSQIFh0DVJqhp",
-    "https://youtu.be/g8tquaFDAgI?si=sDNIvlYWFvUnNkJj",
-    "https://youtu.be/1-kC0Wg9C6k?si=9xK3Y-p38nQYH-Pt"]
+// const videoLinks = ["https://youtu.be/B4SX5FFE2XE?si=gZxq4NxgXJlZehiZ",
+//     "https://youtu.be/vnFybzZtVoQ?si=s05FnYS84z6D7Uxn",
+//     "https://videos.ctfassets.net/7907dlxc16yt/5DgixgwMT6CFWQjl44xtHR/5ce9a2a47985f6d03b33c691523e4d92/-VideoAsk_____WebRevamp_____03-_x2_optimized.mp4",
+//     "https://youtu.be/bz_DPnBEffw?si=-O_eSQIFh0DVJqhp",
+//     "https://youtu.be/1-kC0Wg9C6k?si=9xK3Y-p38nQYH-Pt",
+//     "https://youtu.be/g8tquaFDAgI?si=sDNIvlYWFvUnNkJj",
+// ]
 
-const generateRandomOptions = (depth: number): Option[] => {
-    const options: Option[] = [];
-    for (let i = 0; i < depth; i++) {
-        options.push({
-            text: `Option-${i}`,
-            vidio: i < depth - 1 ? {
-                url: videoLinks[Math.floor(Math.random() * 4)],
-                options: generateRandomOptions(depth - 1)
-            } : null
-        });
-    }
-    return options;
-}
+// const generateRandomOptions = (depth: number): Option[] => {
+//     const options: Option[] = [];
+//     for (let i = 0; i < depth; i++) {
+//         options.push({
+//             text: `Option-${i}`,
+//             vidio: i < depth - 1 ? {
+//                 url: videoLinks[Math.floor(Math.random() * 5)],
+//                 options: generateRandomOptions(depth - 1)
+//             } : null
+//         });
+//     }
+//     return options;
+// }
 
-const generateRandomVideos = (num: number, depth: number): Video[] => {
-    const videos: Video[] = [];
-    for (let i = 0; i < num; i++) {
-        videos.push({
-            url: 'https://videos.ctfassets.net/7907dlxc16yt/5DgixgwMT6CFWQjl44xtHR/5ce9a2a47985f6d03b33c691523e4d92/-VideoAsk_____WebRevamp_____03-_x2_optimized.mp4',
-            // url: 'https://youtu.be/oZDwcrWRQxw?si=jvajGYMwABwzdJo_',
-            options: generateRandomOptions(depth)
-        });
-    }
-    return videos;
-}
+// const generateRandomVideos = (num: number, depth: number): Video[] => {
+//     const videos: Video[] = [];
+//     for (let i = 0; i < num; i++) {
+//         videos.push({
+//             url: 'https://videos.ctfassets.net/7907dlxc16yt/5DgixgwMT6CFWQjl44xtHR/5ce9a2a47985f6d03b33c691523e4d92/-VideoAsk_____WebRevamp_____03-_x2_optimized.mp4',
+//             // url: 'https://youtu.be/oZDwcrWRQxw?si=jvajGYMwABwzdJo_',
+//             options: generateRandomOptions(depth)
+//         });
+//     }
+//     return videos;
+// }
 const Ask: FC<AskProps> = () => {
-    const videos: Video[] = generateRandomVideos(5, 3);
-    const [url, setUrl] = useState<string>(videos[0].url);
+    // const videos: Video[] = generateRandomVideos(5, 3);
+    const videos: Video = {
+        url: "https://www.youtube.com/watch?v=mPymRFeTJa4",
+        options: [
+            {
+                text: "Dolly",
+                vidio: {
+                    url: "https://youtu.be/H6guS2OR_TY?si=-3mt5iZt7vSeaHxP",
+                    options: [
+                        {
+                            text: "7liwa",
+                            vidio: {
+                                url: "https://www.youtube.com/watch?v=7ZHfD4NfgHU",
+                                options: [
+                                    {
+                                        text: "relax",
+                                        vidio: {
+                                            url: "https://www.youtube.com/watch?v=oVzt0ZwIC84",
+                                            options: [
+                                                {
+                                                    text: null,
+                                                    vidio: null
+                                                },]
+                                        }
+                                    },]
 
+                            }
+                        }
+                    ]
+                }
+            }
+            ,
+            {
+                text: "random",
+                vidio: {
+                    url: "https://youtu.be/B4SX5FFE2XE?si=gZxq4NxgXJlZehiZ",
+                    options: [
+                        {
+                            text: "7liwa",
+                            vidio: {
+                                url: "https://www.youtube.com/watch?v=7ZHfD4NfgHU",
+                                options: [
+                                    {
+                                        text: "null",
+                                        vidio: null
+                                    },]
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    };
+    const [url, setUrl] = useState<string>(videos.url);
+    const [options, setOptions] = useState<any>(videos.options);
+
+    const nextNode = (url:string) => {
+        setUrl(url);
+        //set next options for new url
+        const options = videos.options.find((option: Option) => option.vidio?.url === url); //search for the next options by id is pefect
+        setOptions(options?.vidio?.options || []);
+    }
 
 
     return (
-        <div className="flex flex-1 flex-col items-center gap-4 justify-center ">
-            {/* <h1>Ask  {url}</h1> */}
-            <div className=" relative w-full flex h-full justify-center items-center ">
-                {/* <video className="w-full" autoPlay loop muted src={url}></video> */}
-                <DynamicReactPlayer
-                    url={url}
-                    autoplay={true}
-                />
-
-                <div className=" absolute top-0 bottom-5 right-0 left-0 flex justify-end items-center flex-col  gap-2">
-                    {
-                        videos[0].options.map((option, index) => {
-                            return (
-                                option.vidio?.url &&
-                                <div key={index}
-                                    onClick={() => {
-                                        if (option.vidio?.url)
-                                            setUrl(option.vidio?.url || '');
-                                       
-                                    }}
-                                    className="w-1/3 flex justify-center px-3 py-2 rounded-full bg-inherit text-purple-600 border-purple-500 border
-                                hover:bg-purple-600 hover:text-white hover:border-transparent transition-all duration-500 ease-in-out cursor-pointer">
-                                    {option.text}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-
-            </div>
-        </div>
+        <Player url={url} options={options} setUrl={nextNode} />
     );
 }
 
 export default Ask
-
-
-
-{
-    /*
-    
-    interface Video {
-    url: string;
-    options: Option[];
-}
-
-interface Option {
-    text: string;
-    vidio: Video | null;
-}
-
-interface AskProps {
-    videos: Video[];
-}
-
-const generateRandomOptions = (depth: number): Option[] => {
-    const options: Option[] = [];
-    for (let i = 0; i < depth; i++) {
-        options.push({
-            text: `Option ${i}`,
-            vidio: i < depth - 1 ? {
-                url: `https://www.youtube.com/watch?v=${Math.random().toString(36).substring(7)}`,
-                options: generateRandomOptions(depth - 1)
-            } : null
-        });
-    }
-    return options;
-}
-
-const generateRandomVideos = (num: number, depth: number): Video[] => {
-    const videos: Video[] = [];
-    for (let i = 0; i < num; i++) {
-        videos.push({
-            url: `https://www.youtube.com/watch?v=${Math.random().toString(36).substring(7)}`,
-            options: generateRandomOptions(depth)
-        });
-    }
-    return videos;
-}
-const  videos: Video[] = generateRandomVideos(5, 3); 
-console.log(videos)
-// for (const video of videos) {
-//     console.log(video.url);
-//     for (const option of video.options) {
-//         console.log(option.text);
-//         if (option.vidio) {
-//             console.log(option.vidio.url);
-//             for (const option2 of option.vidio.options) {
-//                 console.log(option2.text);
-//                 if (option2.vidio) {
-//                     console.log(option2.vidio.url);
-//                     for (const option3 of option2.vidio.options) {
-//                         console.log(option3.text);
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-    
-    */
-}
--- videoAsk/src/app/(components)/ask/player.tsx
+++ videoAsk/src/app/(components)/ask/player.tsx
@@ -1,47 +1,56 @@
-// import { FC } from "react";
-// import dynamic from 'next/dynamic';
+import { FC } from "react";
+import dynamic from 'next/dynamic';
 
-// const DynamicReactPlayer = dynamic(() => import('react-player'), { ssr: false });
-// interface IPlayer {
-//     url: string;
-//     options: string[];
-// }
+const DynamicReactPlayer = dynamic(() => import('react-player'), { ssr: false });
+interface Video {
+    url: string;
+    options: Option[];
+}
 
-// const Player: FC<IPlayer> = ({ url, options }) => {
-//     return (
-//         <div className="flex flex-1 flex-col items-center gap-4 justify-center ">
-//             {/* <h1>Ask  {url}</h1> */}
-//             <div className=" relative w-full flex h-full justify-center items-center ">
-//                 {/* <video className="w-full" autoPlay loop muted src={url}></video> */}
-//                 <DynamicReactPlayer
-//                     url={url}
-//                     autoplay={true}
-//                 />
+interface Option {
+    text: string;
+    vidio: Video | null;
+}
+interface IPlayer {
+    url: string;
+    options: Option[];
+    setUrl?: any;
+}
 
-//                 <div className=" absolute top-0 bottom-5 right-0 left-0 flex justify-end items-center flex-col  gap-2">
-//                     {
-//                         videos[0].options.map((option, index) => {
-//                             return (
-//                                 option.vidio?.url &&
-//                                 <div key={index}
-//                                     onClick={() => {
-//                                         if (option.vidio?.url)
-//                                             setUrl(option.vidio?.url || '');
-                                       
-//                                     }}
-//                                     className="w-1/3 flex justify-center px-3 py-2 rounded-full bg-inherit text-purple-600 border-purple-500 border
-//                                 hover:bg-purple-600 hover:text-white hover:border-transparent transition-all duration-500 ease-in-out cursor-pointer">
-//                                     {option.text}
-//                                 </div>
-//                             )
-//                         })
-//                     }
-//                 </div>
 
-//             </div>
-//         </div>
-               
-//     );
-// };
+const Player: FC<IPlayer> = ({ url, options , setUrl}) => {
+    return (
+        <div className="flex flex-1 flex-col items-center gap-4 justify-center ">
+            <div className=" relative w-full flex h-full justify-center items-center ">
+                <DynamicReactPlayer
+                    url={url}
+                    autoPlay={true}
+                    playing={true}
+                />
 
-// export default Player;
+                <div className=" absolute top-0 bottom-5 right-0 left-0 flex justify-end items-center flex-col  gap-2">
+                    {
+                        options.map((option, index) => {
+                            return (
+                                option.vidio?.url &&
+                                <div key={index}
+                                    onClick={() => {
+                                        if (option.vidio?.url)
+                                            setUrl(option.vidio?.url || '');
+
+                                    }}
+                                    className="w-1/3 flex justify-center px-3 py-2 rounded-full bg-inherit text-purple-600 border-purple-500 border
+                                hover:bg-purple-600 hover:text-white hover:border-transparent transition-all duration-500 ease-in-out cursor-pointer">
+                                    {option.text}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Player;
